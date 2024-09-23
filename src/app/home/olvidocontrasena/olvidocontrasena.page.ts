dceaf1f5868b4576d8aabe7df20a0cc9313eb10b
Javiera Alvarez <149703653+JavieraFAC@users.noreply.github.com>
2024-09-23 00:28:24 -0300

    Add files via upload

--- src/app/home/olvidocontrasena/olvidocontrasena.page.ts
+++ src/app/home/olvidocontrasena/olvidocontrasena.page.ts
@@ -30,4 +30,44 @@ export class OlvidocontrasenaPage implements AfterViewInit {
     // Ejecutamos la animación
     cardAnimation.play();
   }
-}
+
+  opcionSeleccionada: string = 'telefono';  // Por defecto seleccionamos teléfono
+  telefono: string = '';
+  correo: string = '';
+
+
+  // Validar teléfono chileno
+  isPhoneValid(): boolean {
+    const phoneRegex = /^9[0-9]{8}$/;  // Expresión regular para teléfonos de Chile
+    return phoneRegex.test(this.telefono);
+  }
+
+  // Validar que el correo sea válido
+  isEmailValid(): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  // Expresión regular para validar correos electrónicos
+    return emailRegex.test(this.correo);
+  }
+
+  // Enviar mensaje de recuperación por teléfono
+  enviarMensaje() {
+    if (this.isPhoneValid()) {
+      console.log('Enviando mensaje de recuperación al teléfono...');
+      // Lógica para enviar el mensaje
+    } else {
+      console.log('Número de teléfono no válido');
+    }
+  }
+
+  // Enviar mensaje de recuperación por correo
+  enviarMensajeCorreo() {
+    if (this.isEmailValid()) {
+      console.log('Enviando mensaje de recuperación al correo...');
+      // Lógica para enviar el mensaje
+    } else {
+      console.log('Correo electrónico no válido');
+    }
+  }
+  }
+  
+
+
